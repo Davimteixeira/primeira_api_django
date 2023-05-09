1dc610e30d91ebe5238ff2ebd54a0094fe5b1924
Davimteixeira <daviteixeira077@gmail.com>
2023-05-09 14:58:54 -0300

    autenticando a segurança da api e fazendo busca por id

--- escola/serializer.py
+++ escola/serializer.py
@@ -1,13 +1,36 @@
 from rest_framework import serializers
-from escola.models import Aluno, Curso
+from escola.models import Aluno, Curso, Matricula
 
 class AlunoSerializer(serializers.ModelSerializer):
     class Meta: 
         model = Aluno
         fields = ['id', 'nome', 'rg', 'cpf', 'data_nascimento']
+        # ele ta determinando quais campos ele quer trazer com esse []
         
 
 class CursoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Curso
         fields = '__all__'
+        # o campo fields ta trazendos todos os campos com o '__all__'
+        
+class MatriculaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Matricula
+        exclude = []
+        # esse campo exclude trás todos os campos exceto o que ta dentro do cochete, nesse caso ta trazendo todos
+        
+class ListaMatriculasAlunoSerializer(serializers.ModelSerializer):
+    curso = serializers.ReadOnlyField(source='curso.descricao')
+    periodo = serializers.SerializerMethodField()
+    class Meta:
+        model = Matricula
+        fields = ['curso', 'periodo']
+    def get_periodo(self, obj):
+        return obj.get_periodo_display()
+    
+class ListaAlunosMatriculadosSerializer(serializers.ModelSerializer):
+    aluno_nome = serializers.ReadOnlyField(source='aluno.nome')
+    class Meta:
+        model = Matricula
+        fields = ['aluno_nome']
